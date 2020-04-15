9141a4d09b093519c3022eeaa6b7f70b4a829d7d
Gabriel Seals <sealsgj@gmail.com>
2020-04-15 09:43:37 -0500

    fin

--- BadSuperBowlNamer.Tests/int_to_roman_numerals.cs
+++ BadSuperBowlNamer.Tests/int_to_roman_numerals.cs
@@ -71,6 +71,7 @@ namespace BadSuperBowlNamer.Tests
 
             Assert.Equal(expectedResult, actualResult);
         }
+
         [Fact]
         public void translating_14_results_in_XIV()
         {
@@ -86,5 +87,205 @@ namespace BadSuperBowlNamer.Tests
 
             Assert.Equal(expectedResult, actualResult);
         }
+
+        [Fact]
+        public void translating_29_results_in_XXIX()
+        {
+            var number = 29;
+            var expectedResult = "XXIX";
+            //below this line stays the same
+            var translator = new Translator();
+            var actualResult = translator.Translate(number);
+            Assert.Equal(expectedResult, actualResult);
+        }
+
+
+        [Fact]
+        public void translating_44_results_in_XLIV()
+        {
+            var number = 44;
+            var expectedResult = "XLIV";
+            //below this line stays the same
+            var translator = new Translator();
+            var actualResult = translator.Translate(number);
+            Assert.Equal(expectedResult, actualResult);
+        }
+
+        [Fact]
+        public void translating_45_results_in_XLV()
+        {
+            var number = 45;
+            var expectedResult = "XLV";
+            //below this line stays the same
+            var translator = new Translator();
+            var actualResult = translator.Translate(number);
+            Assert.Equal(expectedResult, actualResult);
+        }
+
+        [Fact]
+        public void translating_68_results_in_LXVIII()
+        {
+            var number = 68;
+            var expectedResult = "LXVIII";
+            //below this line stays the same
+            var translator = new Translator();
+            var actualResult = translator.Translate(number);
+            Assert.Equal(expectedResult, actualResult);
+        }
+
+        [Fact]
+        public void translating_83_results_in_LXXXIII()
+        {
+            var number = 83;
+            var expectedResult = "LXXXIII";
+            //below this line stays the same
+            var translator = new Translator();
+            var actualResult = translator.Translate(number);
+            Assert.Equal(expectedResult, actualResult);
+        }
+
+        [Fact]
+        public void translating_97_results_in_XCVII()
+        {
+            var number = 97;
+            var expectedResult = "XCVII";
+            //below this line stays the same
+            var translator = new Translator();
+            var actualResult = translator.Translate(number);
+            Assert.Equal(expectedResult, actualResult);
+        }
+
+        [Fact]
+        public void translating_99_results_in_XCIX()
+        {
+            var number = 99;
+            var expectedResult = "XCIX";
+            //below this line stays the same
+            var translator = new Translator();
+            var actualResult = translator.Translate(number);
+            Assert.Equal(expectedResult, actualResult);
+        }
+
+        [Fact]
+        public void translating_500_results_in_D()
+        {
+            var number = 500;
+            var expectedResult = "D";
+            //below this line stays the same
+            var translator = new Translator();
+            var actualResult = translator.Translate(number);
+            Assert.Equal(expectedResult, actualResult);
+        }
+
+        [Fact]
+        public void translating_501_results_in_DI()
+        {
+            var number = 501;
+            var expectedResult = "DI";
+            //below this line stays the same
+            var translator = new Translator();
+            var actualResult = translator.Translate(number);
+            Assert.Equal(expectedResult, actualResult);
+        }
+
+        [Fact]
+        public void translating_649_results_in_DCXLIX()
+        {
+            var number = 649;
+            var expectedResult = "DCXLIX";
+            //below this line stays the same
+            var translator = new Translator();
+            var actualResult = translator.Translate(number);
+            Assert.Equal(expectedResult, actualResult);
+        }
+
+        [Fact]
+        public void translating_798_results_in_DCCXCVIII()
+        {
+            var number = 798;
+            var expectedResult = "DCCXCVIII";
+            //below this line stays the same
+            var translator = new Translator();
+            var actualResult = translator.Translate(number);
+            Assert.Equal(expectedResult, actualResult);
+        }
+
+        [Fact]
+        public void translating_891_results_in_DCCCXCI()
+        {
+            var number = 891;
+            var expectedResult = "DCCCXCI";
+            //below this line stays the same
+            var translator = new Translator();
+            var actualResult = translator.Translate(number);
+            Assert.Equal(expectedResult, actualResult);
+        }
+
+        [Fact]
+        public void translating_1000_results_in_M()
+        {
+            var number = 649;
+            var expectedResult = "DCXLIX";
+            //below this line stays the same
+            var translator = new Translator();
+            var actualResult = translator.Translate(number);
+            Assert.Equal(expectedResult, actualResult);
+        }
+
+        [Fact]
+        public void translating_1004_results_in_MIV()
+        {
+            var number = 1004;
+            var expectedResult = "MIV";
+            //below this line stays the same
+            var translator = new Translator();
+            var actualResult = translator.Translate(number);
+            Assert.Equal(expectedResult, actualResult);
+        }
+
+        [Fact]
+        public void translating_1006_results_in_MVI()
+        {
+            var number = 1006;
+            var expectedResult = "MVI";
+            //below this line stays the same
+            var translator = new Translator();
+            var actualResult = translator.Translate(number);
+            Assert.Equal(expectedResult, actualResult);
+        }
+
+        [Fact]
+        public void translating_1023_results_in_MXXIII()
+        {
+            var number = 1023;
+            var expectedResult = "MXXIII";
+            //below this line stays the same
+            var translator = new Translator();
+            var actualResult = translator.Translate(number);
+            Assert.Equal(expectedResult, actualResult);
+        }
+
+        [Fact]
+        public void translating_2014_results_in_MMXIV()
+        {
+            var number = 2014;
+            var expectedResult = "MMXIV";
+            //below this line stays the same
+            var translator = new Translator();
+            var actualResult = translator.Translate(number);
+            Assert.Equal(expectedResult, actualResult);
+        }
+
+        [Fact]
+        public void translating_3999_results_in_MMMCMXCIX()
+        {
+            var number = 3999;
+            var expectedResult = "MMMCMXCIX";
+            //below this line stays the same
+            var translator = new Translator();
+            var actualResult = translator.Translate(number);
+            Assert.Equal(expectedResult, actualResult);
+        }
+
     }
 }
--- BadSuperBowlNamer/Translator.cs
+++ BadSuperBowlNamer/Translator.cs
@@ -11,7 +11,42 @@ namespace BadSuperBowlNamer
             var romanNumeral = "";
             while (number > 0)
             {
-                if (number >= 10)
+                if (number >= 1000)
+                {
+                    romanNumeral += "M";
+                    number -= 1000;
+                }
+                else if (number >= 900)
+                {
+                    romanNumeral += "CM";
+                    number -= 900;
+                }
+                else if (number >= 500)
+                {
+                    romanNumeral += "D";
+                    number -= 500;
+                }
+                else if (number >= 100)
+                {
+                    romanNumeral += "C";
+                    number -= 100;
+                }
+                else if (number >= 90)
+                {
+                    romanNumeral += "XC";
+                    number -= 90;
+                }
+                else if (number >= 50)
+                {
+                    romanNumeral += "L";
+                    number -= 50;
+                }
+                else if (number >= 40)
+                {
+                    romanNumeral += "XL";
+                    number -= 40;
+                }
+                else if (number >= 10)
                 {
                     romanNumeral += "X";
                     number -= 10;
